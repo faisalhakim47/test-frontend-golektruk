a6c8e79674831916a552de578cb2ea8f473ba46f
Faisal Hakim <faisalhakim47@gmail.com>
2024-11-13 20:37:02 +0700

    Soal1 (Done)

--- src/soal/soal1.tsx
+++ src/soal/soal1.tsx
@@ -1,11 +1,10 @@
 import { SyntheticEvent, useCallback, useEffect, useState } from 'react';
-import { assertInstanceOf } from '../tools/assert.ts';
 
 const Soal1 = () => {
   // 1. Buat kotak dibawah menjadi elemen drag and drop tanpa menggunakan plugin
   return (
     <>
-      <Solusi />
+      <Solusi1 />
 
       {/* Ekspektasi hasil */}
       <iframe src="/soal1.mp4" style={{
@@ -18,66 +17,81 @@ const Soal1 = () => {
   )
 }
 
-const Solusi = () => {
+const Solusi1 = () => {
   const [left, setLeft] = useState(0);
   const [top, setTop] = useState(0);
 
-  const [isDragging, setIsDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
-  const [startY, setStartY] = useState(0);
   const [movementX, setMovementX] = useState(0);
   const [movementY, setMovementY] = useState(0);
+
+  // State ini tidak reactive
+  // Hanya digunakan untuk menyimpan state drag tanpa mempengaruhi render
+  const [dragState] = useState({
+    isDragging: false,
+    top: 0,
+    left: 0,
+    startX: 0,
+    startY: 0,
+    movementX: 0,
+    movementY: 0,
+  });
 
   // Ambil posisi awal
   const handleMouseDown = useCallback((event: SyntheticEvent) => {
     assertInstanceOf(event.nativeEvent, MouseEvent);
-    setIsDragging(true);
-    setStartX(event.nativeEvent.clientX);
-    setStartY(event.nativeEvent.clientY);
-  }, []);
+    dragState.isDragging = true;
+    dragState.top = top;
+    dragState.left = left;
+    dragState.startY = event.nativeEvent.clientY;
+    dragState.startX = event.nativeEvent.clientX;
+    dragState.movementY = 0;
+    dragState.movementX = 0;
+  }, [dragState, top, left]);
 
-  // Selisihkan posisi awal dengan pergerakan mouse saat drag
-  // const handleMouseMove = useCallback((event: SyntheticEvent) => {
-  //   assertInstanceOf(event.nativeEvent, MouseEvent);
-  //   if (isDragging) {
-  //     setMovementX(event.nativeEvent.clientX - startX);
-  //     setMovementY(event.nativeEvent.clientY - startY);
-  //   }
-  // }, [isDragging, startX, startY]);
-
+  // Kenapa bind event ke document? Karena kecepatan cursor bisa melebihi kecepatan render
+  // sehinga cursor bisa keluar dari elemen yang di-drag
   useEffect(() => {
+    // Selisihkan posisi awal dengan pergerakan mouse saat drag
     const handleMouseMove = (event: unknown) => {
       assertInstanceOf(event, MouseEvent);
-      if (isDragging) {
-        setMovementX(event.clientX - startX);
-        setMovementY(event.clientY - startY);
+      if (dragState.isDragging) {
+        // update realtime style posisi elemen
+        setMovementX(event.clientX - dragState.startX);
+        setMovementY(event.clientY - dragState.startY);
+        // update state untuk perhitungan posisi akhir
+        dragState.movementX = event.clientX - dragState.startX;
+        dragState.movementY = event.clientY - dragState.startY;
       }
     };
+
+    // Update posisi ke terakhir, dan reset state drag
+    const handleMouseUp = (event: unknown) => {
+      assertInstanceOf(event, MouseEvent);
+
+      setTop(dragState.top + dragState.movementY);
+      setLeft(dragState.left + dragState.movementX);
+
+      setMovementY(0);
+      setMovementX(0);
+      dragState.isDragging = false;
+      dragState.startY = 0;
+      dragState.startX = 0;
+      dragState.movementY = 0;
+      dragState.movementX = 0;
+    };
+
     document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+
     return () => {
       document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
     };
-  // Kenapa tidak dependency ke isDragging?
-  // Kita tidak perlu melisten isDragging karena hanya digunakan untuk checking.
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Terapkan position akhir dan reset drag state
-  const handleMouseUp = useCallback(() => {
-    setIsDragging(false);
-    setLeft(left + movementX);
-    setTop(top + movementY);
-    setStartX(0);
-    setStartY(0);
-    setMovementX(0);
-    setMovementY(0);
-  }, [left, top, movementX, movementY]);
+  }, [dragState]);
 
   return (
     <div
       onMouseDown={handleMouseDown}
-      // onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}
       style={{
         backgroundColor: '#fff',
         width: 40,
@@ -92,3 +106,10 @@ const Solusi = () => {
 };
 
 export default Soal1
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function assertInstanceOf<T>(value: unknown, constructor: new (...args: any[]) => T, message?: string): asserts value is T {
+  if (!(value instanceof constructor)) {
+    throw new Error(message || `Expected value to be an instance of ${constructor.name}`);
+  }
+}
--- src/tools/assert.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function assertInstanceOf<T>(value: unknown, constructor: new (...args: any[]) => T, message?: string): asserts value is T {
-  if (!(value instanceof constructor)) {
-    throw new Error(message || `Expected value to be an instance of ${constructor.name}`);
-  }
-}
