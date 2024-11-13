567a112447b2cbd75af4ccc24cc32a2714fbbe28
Faisal Hakim <faisalhakim47@gmail.com>
2024-11-13 20:37:18 +0700

    Soal3 (Done)

--- src/soal/soal3.tsx
+++ src/soal/soal3.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Soal3() {
   /**
@@ -6,32 +6,61 @@ export default function Soal3() {
    * ? 2. tampilkan data yang di panggil dari api tersebut...
    */
 
-  return <SeachComponent />;
+  return <SearchComponent />;
 }
 
-function SeachComponent() {
+/**
+ * Ada konteks yang masih kurang jelas pada soal ini.
+ * Pada komponen ini terdapat pencarian text yang saya asumsikan adalah pencarian nama foto.
+ * Namun, endpoint api tersebut tidak menyediakan query pencarian, yang ada adalah endpoint untuk mendapatkan data foto berdasarkan id.
+ * Jadi, solusi yang saya ambil:
+ * - Tampilkan semua data foto yang ada pada endpoint tersebut.
+ * - Fitur pencarian akan dilakukan pada frontend.
+ */
+function SearchComponent() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Array<Photo>>([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${search.id}`);
-      const data = await response.json();
+    const abortController = new AbortController();
 
-      setResults(data);
-    }
+    fetch('https://jsonplaceholder.typicode.com/photos', { method: 'GET', signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => {
+        validatePhotosResponse(data);
+        setResults(data);
+      });
 
-    if (search) fetchData();
-  }, [search]);
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  const filteredResults = useMemo(() => {
+    return results.filter((result) => result.title.includes(search));
+  }, [results, search]);
 
   return (
     <div>
       <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search..." />
       <ul>
-        {results.map((result) => (
-          <li key={result.id}>{result.name}</li>
+        {filteredResults.map((result) => (
+          <li key={result.id} style={{ color: "white" }}>{result.title}</li>
         ))}
       </ul>
     </div>
   );
+}
+
+type Photo = {
+  id: number;
+  title: string;
+}
+
+function validatePhotosResponse(data: unknown): asserts data is Array<Photo> {
+  if (!Array.isArray(data)) throw new Error("Data is not an array");
+  for (const item of data) {
+    if (typeof item.id !== "number") throw new Error("ID is not a number");
+    if (typeof item.title !== "string") throw new Error("Title is not a string");
+  }
 }
