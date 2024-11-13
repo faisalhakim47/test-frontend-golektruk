10581bbf88d0753f8646abbade669ad7ac52379d
Faisal Hakim <faisalhakim47@gmail.com>
2024-11-13 21:27:33 +0700

    Soal5 (Done)

--- src/soal/soal5.tsx
+++ src/soal/soal5.tsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { SyntheticEvent, useCallback, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Soal5 = () => {
   // 5. buatlah fungsi untuk menutup modal ketika tombol back browser diklik
-  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  /**
+   * Solusi favorit saya untuk requirement ini adalah dengan menggunakan hash URL
+   * Alasan utamanya adalah mekanisme ini align dengan spesifikasi HTML dan Web API
+   * Pada dasarnya hash URL digunakan untuk navigasi ke section tertentu pada suatu halaman HTML
+   * Nah, kita bisa memanfaatkan hash URL untuk menampilkan section tertentu seperti modal dengan menambahkan hash tertentu seperti `#modal`
+   * Dengan begitu navigasi back dan forward browser dapat bekerja sesuai ekspektasi
+   * Tidak perlu melakukan manipulasi history API
+   */
+
+  const locationHash = useLocationHash();
+  const isModalOpen = locationHash.includes('modal');
+
+  const handleBackButton = useCallback((event: SyntheticEvent) => {
+    if (isModalOpen) {
+      assertInstanceOf(event.nativeEvent, Event);
+      event.nativeEvent.preventDefault();
+      window.history.back();
+    }
+  }, [isModalOpen]);
 
   return (
     <>
       <div style={{ margin: "1rem" }}>
-        {openModal && <Modal />}
-        <button
+        <Modal />
+        <a
+          href="#modal"
           style={{ padding: "2px 4px", background: "white" }}
-          onClick={() => {
-            setOpenModal((prev) => !prev);
-          }}
-        >
-          {openModal ? "close" : "open"} modal
-        </button>
+          onClick={handleBackButton}
+        >{isModalOpen ? "Close" : "Open"}</a>
       </div>
 
       {/* Ekspektasi hasil */}
@@ -36,13 +52,42 @@ const Soal5 = () => {
 const Modal = () => {
   const modalRoot = document.getElementById("modal-root");
 
+  const locationHash = useLocationHash();
+  const isModalOpen = locationHash.includes('modal');
+
   if (!modalRoot) return <></>;
+
   return ReactDOM.createPortal(
-    <section style={{ background: "#8f9cb0", padding: "3rem", position: "fixed", margin: "6rem" }}>
+    <dialog open={isModalOpen} style={{ background: "#8f9cb0", padding: "3rem", position: "fixed", margin: "6rem" }}>
       <div>This is modal</div>
-    </section>,
+    </dialog>,
     modalRoot
   );
 };
+
+function useLocationHash() {
+  const [locationHash, setLocationHash] = useState<string>(window.location.hash);
+
+  // Listen to hash change
+  useEffect(() => {
+    const handleHashChange = () => {
+      setLocationHash(window.location.hash);
+    };
+    window.addEventListener("hashchange", handleHashChange);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  });
+
+  return locationHash;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function assertInstanceOf<T>(value: unknown, constructor: new (...args: any[]) => T, message?: string): asserts value is T {
+  if (!(value instanceof constructor)) {
+    throw new Error(message || `Expected value to be an instance of ${constructor.name}`);
+  }
+}
+
 
 export default Soal5;
